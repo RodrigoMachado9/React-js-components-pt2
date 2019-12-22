5320df555c4aa9714a2b562282b391a611d08377
RodrigoMachado9 <rodrigo.machado3.14@hotmail.com>
2019-12-21 23:45:09 -0300

    ajustes

--- src/App.js
+++ src/App.js
@@ -18,13 +18,15 @@ class App extends Component{
         this.state = INITIAL_STATE;
     }
 
+    // >>>>>                                    Esse componente é executado, logo quando o component é exibido na tela.
     // up of server for simulate data bank...
     // sudo npm install -g @treinaweb/tw-dev-server
+    // get free videos and images in coverr.co
     // url: https://treinaweb.github.io/tw-dev-server/
     async componentDidMount() {
         const videos =  await VideoService.list();
         this.setState({videos});
-        this.selectVideo(videos[0]);
+        this.selectVideo(videos[2]);
     }
 
     selectVideo (video){
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,24 +3,21 @@ import React from "react";
 
 
 function VideoList(props) {
+    const videos = props.videos || []; // caso não exista objeto, então array vazio.
+
+
     return(
         <ul className={'video-list'}>
-            <li className={'video'}>
-                <img src={'https://storage.coverr.co/thumbnails/coverr-raindrops--1572170453303'} alt={'coverr-raindrops--1572170453303'} />
-                <div>Cover-Raind-Rops</div>
-            </li>
-            <li className={'video'}>
-                <img src={'https://storage.coverr.co/thumbnails/coverr-hills-landscape-1568652619375'} alt={'coverr-hills-landscape-1568652619375'} />
-                <div>Cover-Hills-Landscap</div>
-            </li>
-            <li className={'video'}>
-                <img src={'https://storage.coverr.co/thumbnails/coverr-mountains-view-1563376688037'} alt={'coverr-mountains-view-1563376688037'} />
-                <div>Cover-Mountains-View</div>
-            </li>
-            <li className={'video'}>
-                <img src={'https://storage.coverr.co/thumbnails/coverr-amalfi-coast-1570371443102'} alt={'coverr-amalfi-coast-1570371443102'} />
-                <div>Cover-Amalfi-Coast</div>
-            </li>
+            {
+                videos.map(video =>(
+                    <li className={'video'}>
+                        <img src={video.img} alt={video.name} />
+                        <div>{video.name}</div>
+                    </li>
+
+                ))
+
+            }
         </ul>
 
     )
